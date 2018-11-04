832b0ff1565898f74dc3910b0d224a01a1b9e8d6
kuailexiaoyi <409718341@qq.com>
2018-11-04 08:45:24 +0800

    第 14 课 SpringBoot集成Memcached

---
+++ SpringBootBlogDemo/14_SpringBootMemcached/src/main/java/com/springboot/memcached/config/MemcacheConfiguration.java
@@ -0,0 +1,58 @@
+package com.springboot.memcached.config;
+
+import com.danga.MemCached.MemCachedClient;
+import com.danga.MemCached.SockIOPool;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: zrblog
+ * @CreateTime: 2018-10-08 09:05
+ * @Version:v1.0
+ */
+@Configuration
+public class MemcacheConfiguration {
+
+    @Value("${memcache.servers}")
+    private String[] servers;
+    @Value("${memcache.failover}")
+    private boolean failover;
+    @Value("${memcache.initConn}")
+    private int initConn;
+    @Value("${memcache.minConn}")
+    private int minConn;
+    @Value("${memcache.maxConn}")
+    private int maxConn;
+    @Value("${memcache.maintSleep}")
+    private int maintSleep;
+    @Value("${memcache.nagel}")
+    private boolean nagel;
+    @Value("${memcache.socketTO}")
+    private int socketTO;
+    @Value("${memcache.aliveCheck}")
+    private boolean aliveCheck;
+
+    @Bean
+    public SockIOPool sockIOPool() {
+        SockIOPool pool = SockIOPool.getInstance();
+        pool.setServers(servers);
+        pool.setFailover(failover);
+        pool.setInitConn(initConn);
+        pool.setMinConn(minConn);
+        pool.setMaxConn(maxConn);
+        pool.setMaintSleep(maintSleep);
+        pool.setNagle(nagel);
+        pool.setSocketTO(socketTO);
+        pool.setAliveCheck(aliveCheck);
+        pool.initialize();
+        return pool;
+    }
+
+    @Bean
+    public MemCachedClient memCachedClient() {
+        MemCachedClient memCachedClient = new MemCachedClient();
+        return memCachedClient;
+    }
+}
